9d609d2989729227b896e0c9c8ceeea5088227ab
Mike Holly <mikejholly@gmail.com>
2016-01-22 18:10:03 -0800

    Improved test coverage

--- spec/crapidocs/formatter_spec.rb
+++ spec/crapidocs/formatter_spec.rb
@@ -1,3 +1,11 @@
 describe CrapiDocs::Formatter do
+  let(:session) { double }
+  subject { CrapiDocs::Formatter.new(session) }
 
+  describe '#anchor' do
+    it 'creates an link anchor with all special chars removed' do
+      result = subject.send(:anchor, 'foo-bar_1@')
+      expect(result).to eq 'foobar1'
+    end
+  end
 end
--- spec/crapidocs_spec.rb
+++ spec/crapidocs_spec.rb
@@ -67,4 +67,56 @@ describe CrapiDocs do
       subject.done
     end
   end
+
+  describe '.handle_parallel' do
+    before { subject.start(pattern) }
+
+    before do
+      stub_const('ParallelTests', double)
+      allow(ParallelTests)
+        .to receive(:wait_for_other_processes_to_finish)
+        .and_return(true)
+    end
+
+    context 'is the first parallel test process' do
+      it 'loads other sessions and merges' do
+        expect(subject).to receive(:load_sessions).and_return([])
+        expect(subject.session).to receive(:merge).with([])
+        allow(ParallelTests).to receive(:first_process?).and_return(true)
+        subject.send(:handle_parallel)
+      end
+    end
+
+    context 'is another process' do
+      it 'writes out its session data' do
+        allow(ParallelTests).to receive(:first_process?).and_return(false)
+        expect(subject).to receive(:write_session).and_return(true)
+        subject.send(:handle_parallel)
+      end
+    end
+  end
+
+  describe '.session_file' do
+    it 'generates a file name for the test env' do
+      result = subject.send(:session_file)
+      expect(result).to eq './tmp/crapi-session.1'
+    end
+
+    context 'another test number' do
+      it 'generates a file name for the test env' do
+        ENV['TEST_ENV_NUMBER'] = '2'
+        result = subject.send(:session_file)
+        expect(result).to eq './tmp/crapi-session.2'
+      end
+    end
+  end
+
+  describe '.write_session' do
+    it 'marshals the session and writes' do
+      expect(subject)
+        .to receive(:write_file)
+        .with('./tmp/crapi-session.2', Marshal.dump(subject.session))
+      subject.send(:write_session)
+    end
+  end
 end
